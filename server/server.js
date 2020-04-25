28a69fe8e27979faffa3a3e2f74e0a1a446e2b21
John Travers <johnk.travers@gmail.com>
2020-04-25 17:32:05 -0600

    Add initial app and server files for running the backend server

---
+++ server/server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}`);
+});
