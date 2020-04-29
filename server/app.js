d3dd7c7d4534ced30e18a7d9fd185f7d2721a5a7
John Travers <johnk.travers@gmail.com>
2020-04-28 22:49:06 -0600

    Configure server to use build from client side React app

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
@@ -12,6 +13,15 @@ app.locals.title = 'Total Being Reset';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'));
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+  })
+}
+
 app.use('/api/v1/videos', videosRouter);
 
 module.exports = app;
